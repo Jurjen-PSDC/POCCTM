84b26de1a15b2fa690691fba3e5d16f0516d1b52
Jurjen Pieters <jurjen@hotmail.com>
2013-08-12 23:15:54 +0200

    Other data model

--- public/komodels/taskmodel.js
+++ public/komodels/taskmodel.js
@@ -1,7 +1,9 @@
-function TaskModel(name, completed){
+function TaskModel(name, start, finish){
 	var self = this;
 	self.name = name;
-	self.completed = completed;
+	self.completed = false;
+	self.start = start;
+	self.finish = finish;
 }
 
 function TaskListViewModel() {
@@ -9,15 +11,22 @@ function TaskListViewModel() {
     var self = this;
 	
 	self.tasks = ko.observableArray();
-	
-    self.newTaskText = ko.observable();
+	self.newTaskText = ko.observable();
+	self.From = ko.observable();
+	self.Duration = ko.observable();
     self.incompleteTasks = ko.computed(function() {
         return ko.utils.arrayFilter(self.tasks(), function(task) { return !task.completed });
     });
 
+	self.fromToList = new Array("0","1","2", "3", "4", "5", "6", "7", "8", "9");
+	
+	
     // Operations
     self.addTask = function() {
-		var newModel = new TaskModel(this.newTaskText(), false);
+		var finish = parseInt(this.From()) + parseInt(this.Duration());
+		var newModel = new TaskModel(this.newTaskText(), parseInt(this.From()), finish);
+		self.tasks.push(newModel);
+		self.newTaskText("");
 		var taskData = ko.toJSON(newModel);
 		$.ajax({
             url: '/addtask',
@@ -25,14 +34,11 @@ function TaskListViewModel() {
 			contentType: "application/json; charset=utf-8",
             dataType: 'json',
             success: function (data) {
-				self.newTaskText("");
-				//self.tasks.push(new TaskModel(this.newTaskText(), false));
-				console.log("added new task");
+				console.log("added new task : ");
             },
             data: taskData
         });
-
-       };
+	};
     self.removeTask = function(task) { self.tasks.remove(task) };
 	
 	self.loadTasks = function(){
